756997969362fec532f870488fdcedd9a19b6d0f
jsevilla202 <jsevilla@iesnervion.es>
2022-12-13 11:10:46 +0100

    Ej 6 con tablas

--- Tema 5/src/ejercicios/Ej6.java
+++ Tema 5/src/ejercicios/Ej6.java
@@ -1,36 +1,58 @@
 package ejercicios;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ej6 {
 
 	public static void main(String[] args) {
 		int numeros[] = new int[8];
-		int pares[] = new int[8];
-		int impares[] = new int[8];		
+		int numero;
+		int par = 0;
+		int impar = 0;
 		Scanner sc = new Scanner(System.in);
 		
-		for(int i = 0; i<numeros.length; i++) {
-			System.out.print("Inserte un número: ");
-			numeros[i] = sc.nextInt();
-		}
 		for(int i=0; i<numeros.length; i++) {
-			int par=0;
-			int impar=0;
-			if(numeros[i]/2==0) {
-				pares[par]=numeros[i];
+			System.out.print("Inserte un número: ");
+			numero = sc.nextInt();
+			numeros[i] = numero;
+		}
+		
+		for(int i : numeros) {
+			if(i%2==0) {
 				par++;
 			}
 			else {
-				impares[impar]=numeros[i];
 				impar++;
 			}
 		}
-			System.out.print("Los números pares son: ");
-		for(int i = 0; i<pares.length; i++) {
-			System.out.print(pares[i]+" ");
+		int pares[] = new int [par];
+		int impares[] = new int [impar];
+		
+		par = 0;
+		impar = 0;	
+		for(int i : numeros) {
+			if(i%2==0) {
+				pares[par]=i;
+				par++;
+			}
+			if(i%2!=0) {
+				impares[impar]=i;
+				impar++;
+			}
 		}
-			
+		
+		System.out.print("Los pares son ");
+		for(int i = 0; i<pares.length; i++) {
+			System.out.print(pares[i] + " ");
+		}
+		
+		System.out.println();
+		System.out.print("Los impares son ");
+		for(int i = 0; i<impares.length; i++) {
+			System.out.print(impares[i] + " ");
+		}
+		
 		sc.close();
 	}
 
