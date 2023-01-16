b62f68f527d06c7848ac56f8e61810577436c63e
jsevilla202 <jsevilla@iesnervion.es>
2023-01-16 10:20:19 +0100

    Ej 4 resuelto y 6

--- Tema 5/src/ejercicios3/Ej4.java
+++ Tema 5/src/ejercicios3/Ej4.java
@@ -8,7 +8,7 @@ public class Ej4 {
 	public static void main(String[] args) {
 		int numero;
 		int tablaEnteros[] = new int [10];
-		int tablaEnteros2[] = new int [10];
+		int tablaEnteros2[] = new int [0];
 		
 		Scanner sc = new Scanner(System.in);
 		
@@ -23,13 +23,12 @@ public class Ej4 {
 		
 		int j =0;
 		for(int i =0; i<tablaEnteros.length; i++) {
-			int doble = Arrays.binarySearch(tablaEnteros2, 0, i, tablaEnteros[i]);
+			int doble = Arrays.binarySearch(tablaEnteros2, tablaEnteros[i]);
 			if(doble<0) {
-				tablaEnteros2[j] = tablaEnteros[i];
-				j++;
+				tablaEnteros2 = Arrays.copyOf(tablaEnteros2, tablaEnteros2.length+1);
+				tablaEnteros2[tablaEnteros2.length-1] = tablaEnteros[i];			
 			}
 		}
-		
 		
 		System.out.println(Arrays.toString(tablaEnteros2));
 		
--- Tema 5/src/ejercicios3/Ej6.java
+++ Tema 5/src/ejercicios3/Ej6.java
@@ -1,9 +1,27 @@
 package ejercicios3;
 
+import java.util.Arrays;
+
 public class Ej6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int array[] = new int [100];
+		int array2[] = new int [100];
+		
+		for(int i = 1; i<array.length+1; i++) {
+			array[i-1] = i;
+		}
+		
+		for(int i = array.length-1; i>=0 ; i--) {
+			array2[i] = array[array.length-1-i];
+		}
+		
+		System.out.println(Arrays.toString(array));
+		System.out.println(Arrays.toString(array2));
+		
+		
+		
+		
 
 	}
 
