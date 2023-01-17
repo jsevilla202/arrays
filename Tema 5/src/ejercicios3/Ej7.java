cb1e712cb61d6e93b61c8823c381165e798ed853
jsevilla202 <jsevilla@iesnervion.es>
2023-01-17 11:12:40 +0100

    Ej 7

---
+++ Tema 5/src/ejercicios3/Ej7.java
@@ -0,0 +1,56 @@
+package ejercicios3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ej7 {
+
+	public static void main(String[] args) {
+		int valor;
+		int posicion;
+		String opcion = "";
+		int intopcion = 0;
+		int tablaEnteros[] = new int [10];
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(intopcion!=3) {
+			System.out.println("a. Mostrar valores\nb. Introducir valores\nc. Salir");
+			System.out.println("¿Qué acción desea realizar? ");
+			opcion = sc.next();
+			
+			if(opcion.equals("a")) {
+				intopcion = 1;
+			}
+			else if(opcion.equals("b")){
+				intopcion = 2;
+			}
+			else {
+				intopcion = 3;
+			}
+			switch(intopcion) {
+			case 1: System.out.println(Arrays.toString(tablaEnteros)); System.out.println(); break;
+			case 2: 
+				System.out.print("¿Qué valor desea introducir? ");
+				valor = sc.nextInt();
+				System.out.print("¿En cual posición lo desea introducir? ");
+				posicion = sc.nextInt();
+				if(posicion>=10||posicion<0) {
+					System.out.println("ERROR: Posición fuera del limite de la tabla, inserte un valor de entre 0 a "+(tablaEnteros.length-1));
+				}
+				else{
+					tablaEnteros[posicion] = valor;
+					System.out.println("Valor añadido");
+				}
+				System.out.println();
+				break;
+			case 3: break;
+			default: System.out.println("ERROR: Valor no valido"); break;
+			}
+		}
+		
+		sc.close();
+
+	}
+
+}
